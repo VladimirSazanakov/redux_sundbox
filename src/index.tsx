b16c75cc7b54a3a025643317b4fe56ef62e390c0
Vladimir <sazanakov@inbox.ru>
2023-08-18 23:48:20 +0700

    3.4.6

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ const reducer = (state = 0, action: any) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
+    case 'DEC':
+       return state -1;
 
     default:
       return state;
@@ -13,6 +15,28 @@ const reducer = (state = 0, action: any) => {
 
 const store = createStore(reducer);
 
+document
+  .getElementById("inc")
+  ?.addEventListener('click', ()=>{
+    store.dispatch({type: "INC"});
+  });
+
+  document
+    .getElementById('dec')
+    ?.addEventListener('click', ()=>{
+      store.dispatch({type: 'DEC'})
+    })
+
+  const update = () =>{
+    const counter = document.getElementById('counter');
+    if (counter){
+      counter.innerHTML = store.getState().toString();
+    } else {
+      console.log('ups counter element is undefined');
+    }
+  };
+
+  store.subscribe(update);
 store.subscribe(() => console.log(store.getState()));
 
 store.dispatch({ type: 'INC' });
